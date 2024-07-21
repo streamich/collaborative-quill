99cefda9b425a08e4169901003cd9fc0bae18b58
streamich <streamich@gmail.com>
2024-07-21 16:13:57 +0200

    feat: 🎸 add model to editor synce

--- src/QuillBinding.ts
+++ src/QuillBinding.ts
@@ -1,4 +1,5 @@
 import Delta from 'quill-delta';
+import {invokeFirstOnly} from './util';
 import type Quill from 'quill';
 import type {QuillDeltaApi} from 'json-joy/lib/json-crdt-extensions/quill-delta';
 import type {QuillDeltaOp} from 'json-joy/lib/json-crdt-extensions/quill-delta/types';
@@ -10,52 +11,78 @@ export class QuillBinding {
     binding.bind();
     return binding.unbind;
   };
+  
+  protected readonly race = invokeFirstOnly();
 
   constructor(
     public readonly api: QuillDeltaApi,
     public readonly quill: Quill,
   ) {}
 
+  // ----------------------------------------------------- Editor-to-Model sync
+
   private readonly onTextChange: OnTextChange = (delta) => {
-    const ops = delta.ops as QuillDeltaOp[];
-    /**
-     * When inside an annotated text (say bold text), a character is inserted,
-     * just before the last annotated character, and the inserted character
-     * is the same as the last character, then Quill does not insert it in
-     * the right place: it inserts it just after the last annotated character.
-     * This is a workaround for this bug.
-     */
-    if (ops.length === 2) {
-      const retain: number | undefined = (ops[0] as any).retain;
-      const insert: unknown = (ops[1] as any).insert;
-      const attributes = (ops[1] as any).attributes;
-      if (
-        typeof retain === 'number' &&
-        typeof insert === 'string' &&
-        attributes &&
-        insert.length === 1 &&
-        insert !== '\n'
-      ) {
-        const selection = this.quill.getSelection();
-        if (selection && selection.length === 0) {
-          if (selection.index === retain) {
-            delta.ops[0].retain = retain - 1;
+    this.race(() => {
+      const ops = delta.ops as QuillDeltaOp[];
+      /**
+       * When inside an annotated text (say bold text), a character is inserted,
+       * just before the last annotated character, and the inserted character
+       * is the same as the last character, then Quill does not insert it in
+       * the right place: it inserts it just after the last annotated character.
+       * This is a workaround for this bug.
+       */
+      if (ops.length === 2) {
+        const retain: number | undefined = (ops[0] as any).retain;
+        const insert: unknown = (ops[1] as any).insert;
+        const attributes = (ops[1] as any).attributes;
+        if (
+          typeof retain === 'number' &&
+          typeof insert === 'string' &&
+          attributes &&
+          insert.length === 1 &&
+          insert !== '\n'
+        ) {
+          const selection = this.quill.getSelection();
+          if (selection && selection.length === 0) {
+            if (selection.index === retain) {
+              delta.ops[0].retain = retain - 1;
+            }
           }
         }
       }
-    }
-    this.api.apply(ops);
+      this.api.apply(ops);
+    });
   };
+
+  // ----------------------------------------------------- Model-to-Editor sync
+
+  protected syncFromModel(): void {
+    const modelDelta = new Delta(this.api.view());
+    const diff = modelDelta.diff(this.quill.getContents());
+    this.quill.updateContents(diff, 'silent');
+  }
+
+  protected readonly onModelChange = () => {
+    this.race(() => {
+      this.syncFromModel();
+    });
+  };
+
+  // ------------------------------------------------------------------ Binding
+
+  private _s: (() => void) | null = null;
 
   public readonly bind = () => {
     const delta = new Delta(this.api.view());
     const quill = this.quill;
     quill.setContents(delta, 'silent');
     quill.on('text-change', this.onTextChange);
+    this._s = this.api.api.onChange.listen(this.onModelChange);
   };
 
   public readonly unbind = () => {
     const quill = this.quill;
     quill.off('text-change', this.onTextChange);
+    this._s?.();
   };
 }
